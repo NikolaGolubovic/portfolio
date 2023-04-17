c3b648b5e3fc6fd681b97ca2e8a8fec9829a8d2b
NikolaGolubovic <nikolagolubovic985@gmail.com>
2023-04-17 13:13:39 +0200

    remove js image add hello msg

--- src/components/CoverImage.js
+++ src/components/CoverImage.js
@@ -1,10 +1,11 @@
 import React from "react";
 import jsImage from "../images/js-cover-compressed.jpg";
+import { HelloMsg } from "./HelloMsg";
 
 function CoverImage() {
   return (
     <div className="js-image-container">
-      <img src={jsImage} alt="Javascript" className="js-image" />
+      <HelloMsg />
     </div>
   );
 }
